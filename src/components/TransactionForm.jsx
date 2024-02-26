012242045a6b9749d64be67f6a4c9df035f0733c
Jonatandb <jonatandb@gmail.com>
2024-02-26 10:38:54 -0300

    Add Transaction form

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+import { useGlobalState } from "../context/GlobalState"
 
 function TransactionForm() {
+  const { addTransaction } = useGlobalState()
+
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const transaction = {
+      id: 1,
+      description,
+      amount
+    }
+    addTransaction(transaction)
+  }
+
   return (
-    <div>TransactionForm</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder="Enter a description"
+          onChange={e => setDescription(e.target.value)}
+          value={description} />
+        <input type="number"
+          placeholder="0.00"
+          step="0.01"
+          onChange={e => setAmount(e.target.value)}
+          value={amount} />
+        <button>Add Transaction</button>
+      </form>
+    </div>
   )
 }
 
--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,7 +1,12 @@
 export default function AppReducer(state, action) {
   switch (action.type) {
-    case 'ADD_TRANSACTION':
-      return state
+    case 'ADD_TRANSACTION': {
+      const newState = {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      }
+      return newState
+    }
     default:
       return state
   }
