b342c6ae7122e595cbea074378b8c5328203e754
Jonatandb <jonatandb@gmail.com>
2024-02-26 13:46:01 -0300

    Add localStorage persistence

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer } from 'react'
+import { createContext, useContext, useReducer, useEffect } from 'react'
 import AppReducer from './AppReducer'
 
 export const Context = createContext()
@@ -13,7 +13,15 @@ const initialState = {
 }
 
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AppReducer, initialState)
+  const [state, dispatch] = useReducer(AppReducer, initialState,
+    () => {
+      const savedLocalData = localStorage.getItem('transactions')
+      return savedLocalData ? JSON.parse(savedLocalData) : initialState
+    })
+
+  useEffect(() => {
+    localStorage.setItem('transactions', JSON.stringify(state))
+  }, [state])
 
   const addTransaction = transaction => {
     dispatch({
