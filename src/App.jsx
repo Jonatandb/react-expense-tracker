8fd4be60321f50e3cf7c81552899c2abb9a7d0e5
Jonatandb <jonatandb@gmail.com>
2024-02-26 13:35:08 -0300

    Add Income and expenses

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from "./components/Header"
 import Balance from "./components/Balance"
 import TransactionForm from "./components/Transactions/TransactionForm"
 import TransactionList from "./components/Transactions/TransactionList"
+import IncomeExpenses from "./components/IncomeExpenses"
 
 function App() {
   return (
     <GlobalProvider>
       <Header />
+      <IncomeExpenses />
       <Balance />
       <TransactionForm />
       <TransactionList />
---
+++ src/components/IncomeExpenses.jsx
@@ -0,0 +1,32 @@
+import { useGlobalState } from "../context/GlobalState"
+
+function IncomeExpenses() {
+  const { transactions } = useGlobalState()
+  let income = 0
+  let expense = 0
+
+  transactions.forEach(transaction => {
+    if (transaction.amount > 0) {
+      income += transaction.amount
+    } else {
+      expense += transaction.amount
+    }
+  });
+
+  expense = expense * -1
+
+  return (
+    <>
+      <div>
+        <h4>Income</h4>
+        <p>{income}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p>{expense}</p>
+      </div>
+    </>
+  )
+}
+
+export default IncomeExpenses
