ca9c5fd235bbf582686f9509f3a109e8ce7f2b31
Jonatandb <jonatandb@gmail.com>
2024-02-26 18:10:42 -0300

    Add better styling to balance information

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -5,11 +5,11 @@ function Balance() {
 
   const { transactions } = useGlobalState()
   const amounts = transactions.map(t => t.amount)
-  const total = amounts.reduce((acc, amount) => acc + amount, 0)
+  const total = amounts.reduce((acc, amount) => acc + amount, 0).toFixed(2)
 
   return (
-    <div className='flex justify-between'>
-      <h3>Your Balance:</h3>
+    <div className='flex justify-between align-baseline'>
+      <h3 className='leading-8'>Your Balance:</h3>
       <h1 className='text-2xl font-bold'>${total}</h1>
     </div>
   )
